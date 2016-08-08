03a89a679cddc4a9c35f694daab6a28666b7a9a7
unknown <tatashat@gmail.com>
2016-08-08 23:19:31 +0300

    * port

---
+++ app/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var moment = require('moment');
+
+var path = process.cwd();
+var TimestampHandler = require('./timestamp-handler');
+
+var app = express();
+var timestampHandler = new TimestampHandler(moment);
+
+app.get('/', function (req, res) {
+  res.sendFile(path + '/public/index.html');
+});
+
+app.get('/favicon.ico', function (req, res) {
+});
+
+app.get('/:timestamp', timestampHandler.getTime);
+
+var port = 54828;
+app.listen(port);
+
+app.listen(port, function () {
+  console.log(`Timestamp app listening on port ${port}!`);
+});
