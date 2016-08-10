c303c17262c22c37465f888b4489427b38f26f32
unknown <tatashat@gmail.com>
2016-08-10 22:48:19 +0300

    * site address

--- server.js
+++ server.js
@@ -16,26 +16,8 @@ app.get('/favicon.ico', function (req, res) {
 
 app.get('/:timestamp', timestampHandler.getTime);
 
-/*var port = 54828;
-app.listen(port);
-
-app.listen(port, function () {
-  console.log(`Timestamp app listening on port ${port}!`);
-});*/
 var port = process.env.PORT || 1024;
 
 app.listen(port, function() {
   console.log('Listening on ' + port);
 });
-
-/*var http=require('http');
-
-var server=http.createServer(function(req,res){
-    res.end('test');
-});
-
-server.on('listening',function(){
-    console.log('ok, server is running');
-});
-
-server.listen(80);*/
